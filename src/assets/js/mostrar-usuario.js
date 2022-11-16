4f49c01b527734b9aeae7e9e1c5bb0b4b800ffea
JohnnyRamirezSancho <info@johnnyramirez.es>
2022-11-16 15:28:14 +0100

    Mansaje inscripcion newsleter, javaScript

---
+++ src/assets/js/mostrar-usuario.js
@@ -0,0 +1,4 @@
+function mostrarEmail() {
+    let emailUsuario = document.getElementById('input-email').value;
+    alert(`Su email - ${emailUsuario} - ha quedado inscrito en nuestra newsletter.`);
+}
