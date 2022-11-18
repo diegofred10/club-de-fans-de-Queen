93fcc2a426b753679855ba6911c7028b55e1086e
diegofred10 <diegoa.gsm@gmail.com>
2022-11-18 11:02:43 +0100

    LIstado discos y canciones

--- src/assets/js/listado-discos.js
+++ src/assets/js/listado-discos.js
@@ -1,4 +1,8 @@
 export let discos = [
     ['<img src="./assets/img/queen-hot-space.jpg" alt="Hot space">', "Hot space"],
-    ['<img src="./assets/img/queen-a-day-at-the-races.jpg" alt="A day at the races">', "A day at the races"]
+    ['<img src="./assets/img/queen-a-day-at-the-races.jpg" alt="A day at the races">', "A day at the races"],
+    ['<img src="./assets/img/queen-flash-gordon.jpg" alt="Flash Gordon Theme">', "Flash Gordon Theme"],
+    ['<img src="./assets/img/queen-gratest-hits-ii.jpg" alt="Queen gratest hits">', "Queen Gratest Hits"],
+    ['<img src="./assets/img/queen-innuendo.jpg" alt="Innuendo">', "Innuendo"],
+    ['<img src="./assets/img/queen-the-game.jpg" alt="The game">', "The game"]
   ];
